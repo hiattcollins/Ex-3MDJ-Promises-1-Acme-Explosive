68a0a9ba6680c5805819d37269c32ded88887bb8
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-23 18:06:50 -0500

    cleaned up main.js and added comments

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,136 +1,91 @@
 "use strict";
 
-console.log("main.js");
-
-// console.error("busted");
+// console.log("main.js");
 
 $(document).ready(function() {
 
+// ********** DEFINE VARIABLES TO HOLD INCOMING DATA ********** //
 	let holdCategories;
 	let holdTypes;
 	let holdProducts;
 
+
+// ********** LISTEN FOR CHANGE OF CATEGORY ********** //
 $("#select-category").change(function() {
 	let categorySelected = $("#select-category").val();
 
+// ********** CLEAR INCOMING DATA VARIABLES********** //
 	holdCategories = [];
 	holdTypes = [];
 	holdProducts = {};
 
-
-// ********** ********** //
-
 // ********** CLEAR DOM COLUMNS ********** //
-
 		$("#output-row-1").html("");
 		$("#output-row-2").html("");
 		$("#output-row-3").html("");
 
-	console.log("categorySelected", categorySelected);
-
-	
+// ********** LOAD DATA, TRIGGER SORT, AND TRIGGER PRINT TO DOM ********** //
 	loadCategories()
 	.then((loadedCategories) => {
-		console.log("loadedCategories =", loadedCategories);
 		holdCategories = loadedCategories;
-		console.log("holdCategories", holdCategories);
 		return loadTypes();
 	})
 	.then((loadedTypes) => {
-		console.log("loadedTypes =", loadedTypes);
 		holdTypes = loadedTypes;
-		console.log("holdTypes", holdTypes);
-
 		return loadProducts();
 	})
 	.then((loadedProducts) => {
-		console.log("loadedProducts =", loadedProducts);
 		holdProducts = loadedProducts;
-		console.log("holdProducts", holdProducts);
-
 		return sortProducts(categorySelected, holdCategories, holdTypes, holdProducts);
 	})
 	.then((sortedProducts) => {
-		// console.log("loadedProducts in last then:", loadedProducts);
-		console.log("The then after last promise call &&&");
-
-		console.log("sortedProducts", sortedProducts);
-
-
-		// logResults(holdCategories, holdTypes, holdProducts);
-		return printLoadedProducts(sortedProducts);
-		
+		return printLoadedProducts(sortedProducts);		
 	});
 });
 
-function sortProducts (inputCategorySelected, inputCategories, inputTypes, inputProducts) {
 
-	console.log("inputCategorySelected", inputCategorySelected);
-	console.log ("inputCategories.categories", inputCategories.categories);
-	console.log("inputProducts.products", inputProducts.products);
+// ********** FUNCTION TO CONSTRUCT OBJECT OF PRODUCTS IN CHOSEN CATEGORY ********** //
+function sortProducts (inputCategorySelected, inputCategories, inputTypes, inputProducts) {
 	let productArray = [];
 
+// ********** CHOOSE CATEGORY BASED ON DROP-DOWN CHOICE ********** //
 	let selectedCategory = $.grep(inputCategories.categories, (element) => {
 		let categoryName = element.name;
 		console.log("categoryName", categoryName);
 		return (categoryName === inputCategorySelected);
 	}); 
 
-	console.log("selectedCategory", selectedCategory);
-
+// ********** GET ARRAY OF TYPES BY CATEGORY ********** //
 	let selectedCategoryId = selectedCategory[0].id;
-	console.log("selectedCategoryId", selectedCategoryId);
-
 	let selectedTypes = $.grep(inputTypes.types, (element) => {
 		let categoryOfType = element.category;
 		return (categoryOfType === selectedCategoryId);
 	});
 
-	console.log("selectedTypes", selectedTypes);
-
-	console.log("inputProducts.products[0]", inputProducts.products[0]);
-
-
+// ********** CONSTRUCT SELECTED PRODUCS OBJECT BASED ON TYPE AND CATEGORY ********** //
 	let selectedProducts = {};
-
 	$.each(selectedTypes, (typesKey, typesValue) => {
-
 		$.each(inputProducts.products[0], (productsKey, productsValue) => {
-		// console.log("productsKey:", productsKey, "productsValue.id", productsValue.id);
-
-		if (productsValue.type === typesValue.id) {
-			console.log("******* productsValue", productsValue);
-			selectedProducts[productsKey] = productsValue;
-			productsValue["category"] = inputCategorySelected;
-			productsValue["type"] = typesValue.name;
-			productsValue["type_description"] = typesValue.description;
-			// console.log("selectedProducts[productsKey].productsValue", selectedProducts[productsKey].productsValue);
-		}
-
+			if (productsValue.type === typesValue.id) {
+				selectedProducts[productsKey] = productsValue;
+				productsValue["category"] = inputCategorySelected;
+				productsValue["type"] = typesValue.name;
+				productsValue["type_description"] = typesValue.description;
+			}
 		});
-
 	});
-
-	// console.log("selectedProducts", selectedProducts);
-
 	return selectedProducts;
 }
 
 
-
+// ********** PRINT EACH PRODUCT TO DOM BASED ON ROW ID ********** //
 function printLoadedProducts (loadedProducts) {
-
-	console.log("At printLoadedProducts-->");
-
 	let counter = 0;
 	let rowNumber = 1;
 	let rowID;
 
 	$.each(loadedProducts, (key, value) => {
 		rowID = "#output-row-" + rowNumber;
-
-		console.log("rowID", rowID);
-
 		$(rowID).append(`<div class="col-sm-4">
 							<div><h3>${value.name}</h3></div>
 							<div>${value.category}: ${value.type}</div>
@@ -145,59 +100,14 @@ function printLoadedProducts (loadedProducts) {
 	});
 }
 
-// function logResults (input1, input2, input3) {
-// 	console.log("holdCategories:", input1, "holdTypes:", input2, "holdProducts:", input3);
-// }
 
-
-// function printSomething(input1) {
-// 	console.log("printSomething triggered");
-// 	console.log("printSomething input1:", input1);
-// 	$("#output-div").html(`<h2>${input1}</h2>`);
-// }
-
-function logCategories(input1, input2) {
-	console.log("logCategories loadedCats", input1);
-	console.log("logCategories inputCategorySelected", input2);
-
-	// input1.find()
-	// let keys = Object.keys(input1["categories"]);
-	// console.log("keys", keys);
-
-
-	var categoryObj = input1["categories"].find(getCategoryId);
-	//  => {
-	// 	return categoryObj.id
-	// });
-
-	function getCategoryId(categoryObj) {
-		return categoryObj.name === input2;
-	}
-
-	console.log("categoryObj", categoryObj);
-
-	var categoryId = categoryObj["id"];
-	console.log("categoryId", categoryId);
-
-	return categoryId;
-}
-
-// function lastToPrint ()
-
-// function holdCategories (input) {
-
-// }
-
+// ********** LOAD CATEGORIES DATA FROM JSON ********** //
 function loadCategories() {
 	return new Promise( function(resolve, reject) {
 		$.ajax({
 			url: "../javascripts/categories.json"
 		})
 		.done(function(loadedCategories) {
-			console.log("loadedCats right after ajax done", loadedCategories);
-			// var resultingCatId = logCategories(loadedCats, inputCategorySelected);
-			// console.log("resultingCatId right before resolve", resultingCatId);
-			// resolve(resultingCatId);
 			resolve(loadedCategories)
 		})
 		.fail(function(error) {
@@ -206,15 +116,14 @@ function loadCategories() {
 	});
 }
 
+
+// ********** LOAD TYPES DATA FROM JSON ********** //
 function loadTypes(input) {
 	return new Promise( function(resolve, reject) {
 		$.ajax({
 			url: "../javascripts/types.json"
 		})
 		.done(function(loadedTypes) {
-			console.log("loadedTypes right after ajax done", loadedTypes);
-			// var resultingCatId = logCategories(loadedCats, inputCategorySelected);
-			// console.log("resultingCatId right before resolve", resultingCatId);
 			resolve(loadedTypes);
 		})
 		.fail(function(error) {
@@ -223,15 +132,14 @@ function loadTypes(input) {
 	});
 }
 
+
+// ********** LOAD PRODUCTS DATA FROM JSON ********** //
 function loadProducts(input) {
 	return new Promise( function(resolve, reject) {
 		$.ajax({
 			url: "../javascripts/products.json"
 		})
 		.done(function(loadedProducts) {
-			console.log("loadedProducts right after ajax done", loadedProducts);
-			// var resultingCatId = logCategories(loadedCats, inputCategorySelected);
-			// console.log("resultingCatId right before resolve", resultingCatId);
 			resolve(loadedProducts);
 		})
 		.fail(function(error) {
@@ -239,9 +147,6 @@ function loadProducts(input) {
 		});
 	});
 }
-
-
-
 
 
 });
